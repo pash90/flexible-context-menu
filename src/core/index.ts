eac9508cde41d022d5eb637552da5baabf4017b2
Parth Mehta <tech@iamdifferent.io>
2019-08-06 16:42:45 +1000

    🎨 added a note to stop ppl from converting the main handler to es6 notation

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,15 @@
 import { Core } from "cytoscape";
 import { createMenu, hideContextMenu, showContextMenu } from "./context-menu";
 
+/**
+ *
+ * @param {Core} this
+ * @param {MenuItem[]} items
+ *
+ * Note: DO NOT CONVERT THIS FUNCTION TO ES6 OR LATER. ONCE
+ * CONVERTED TO ES6, IT LOOSES THE CONTEXT FOR `this` WHICH
+ * IS REQUIRED TO MAKE AN EXTENSION WORK.
+ */
 export default function extension(this: Core, items: MenuItem[]): Core {
   const menu = createMenu(items);
 
